6209f4d3177827aa92c35690154d4176e5cbdcc3
Kuldeep Paul <kuldeeppaul@Kuldeeps-MacBook-Air.local>
2024-08-26 01:43:43 +0100

    GA tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import Navbar from "@/components/layout/Navbar";
 import {ThemeProvider} from "@/app/theme-provider";
 import Footer from "@/components/layout/Footer";
+import GoogleAnalytics from '@/components/GoogleAnalytics';
+
 const font = Mulish({subsets: ["latin"]});
 
 export const metadata: Metadata = {
@@ -15,6 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en" className="dark">
+            <GoogleAnalytics />
         <body className={font.className}>
         <ThemeProvider>
             <Navbar/>
